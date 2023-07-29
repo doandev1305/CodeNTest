62f45f603170b673c64f8a4c5ca6bc726730e510
DoanDV <doandv@rikkeisoft.com>
2023-07-29 16:07:40 +0700

    allow cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
 const express = require('express');
-const app = express();
-require('dotenv').config()
+const cors = require('cors')
 const router = require('./src/routers')
+const app = express();
 app.use(express.json())
+app.use(cors());
+
+require('dotenv').config()
 
 router(app)
 // Define a route for the homepage
